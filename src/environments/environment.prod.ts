e36b813aa74c2be10797ebee1c91bca566ff1e02
Vedant <vedantarole@rediffmail.com>
2022-09-01 14:41:43 +0530

    production commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDhwaFpHWdx47Ts0vNlTaCZl1dUjguxDTk',
+    authDomain: 'authentication-dcaa5.firebaseapp.com',
+    projectId: 'authentication-dcaa5',
+    storageBucket: 'authentication-dcaa5.appspot.com',
+    messagingSenderId: '95457823883',
+    appId: '1:95457823883:web:aa05bbf8569b86c439988e',
+    measurementId: 'G-40SB812812',
+  },
 };
